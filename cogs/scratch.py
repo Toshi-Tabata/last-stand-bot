06513b2a7d996f13ad474b48b180cd35863fa350
BuildTools <unconfigured@null.spigotmc.org>
2022-03-28 15:27:19 +1100

    update gitignore and scratch file

---
+++ cogs/scratch.py
@@ -0,0 +1,31 @@
+# Scratch File for testing random things
+
+import discord
+from discord.ext import commands
+
+
+class Scratch(commands.Cog):
+    """Scratch class for testing anything"""
+
+    def __init__(self, client):
+        self.client = client
+
+
+    # Actual command definition
+    @commands.command()
+    async def scratch(self, ctx, *msgs):
+        """scratch command for testing things"""
+
+        await ctx.send("hello world")
+
+
+    @commands.command()
+    async def ping(self, ctx, *msgs):
+        """pong!"""
+        print(msgs)
+        await ctx.send("pongers!")
+
+
+
+def setup(client):
+    client.add_cog(Scratch(client))
---
+++ update_from_github.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git -C /home/pi/programming/python/laststand/lastStandBot pull origin
+sudo systemctl restart laststandbot
